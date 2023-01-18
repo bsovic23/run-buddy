dd0f36472e5a05340463222d77835fa11959c05c
Brit Sovic <bsovic@tulane.edu>
2023-01-18 12:19:29 -0500

    updating html routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const htmlRoutes = require('./html/index');
 
 router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
   });
+
+router.use((req, res) => {
+  res.status(404).send('<h1>😝 404 Error!</h1>');
+});
 
 module.exports = router;
